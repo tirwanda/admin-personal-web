041e7968f7f2ba65f2809fa9d6173c1028f01b4b
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-09-11 21:39:07 +0700

    Create user and authority entities

---
+++ be/src/main/java/com/admin/dashboard/be/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.admin.dashboard.be.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class UserController {
+
+    @GetMapping
+    public String testApp() {
+        return "Hello Spring Security";
+    }
+
+}
---
+++ be/src/main/java/com/admin/dashboard/be/entity/User.java
@@ -0,0 +1,100 @@
+package com.admin.dashboard.be.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(
+        name = "AUTH_USER_DETAILS",
+        uniqueConstraints = @UniqueConstraint(
+                name = "email_unique",
+                columnNames = "email_address"
+        )
+)
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    @Column(name = "created_on")
+    private Date createdAt;
+
+    @Column(name = "updated_on")
+    private Date updatedAt;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_address")
+    private String email;
+
+    @ManyToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(
+            name = "AUTH_USER_AUTHORITY",
+            joinColumns = @JoinColumn(referencedColumnName = "userId"),
+            inverseJoinColumns = @JoinColumn(referencedColumnName = "userId")
+    )
+    private List<Authority> authorities;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}

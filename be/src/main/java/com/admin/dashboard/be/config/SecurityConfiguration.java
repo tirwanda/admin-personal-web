e0540e21f6c969df30c89e3f052d7a4e3cbb5d11
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-09-13 14:52:12 +0700

    Add Configuratin for JWT

--- be/src/main/java/com/admin/dashboard/be/config/SecurityConfiguration.java
+++ be/src/main/java/com/admin/dashboard/be/config/SecurityConfiguration.java
@@ -4,12 +4,17 @@ import com.admin.dashboard.be.service.CustomUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +22,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserService useService;
+
+    @Autowired
+    private JWTTokenHelper jwtTokenHelper;
+
+    @Autowired
+    private AuthenticationEntryPoint authenticationEntryPoint;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -29,6 +40,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(useService).passwordEncoder(passwordEncoder());
     }
 
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -36,9 +52,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().anyRequest().permitAll();
-        http.authorizeRequests().anyRequest().authenticated();
+        // http.authorizeRequests().anyRequest().permitAll();
+        // http.authorizeRequests().anyRequest().authenticated();
+        // http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS.)
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint).and()
+                .authorizeRequests((request) -> request.antMatchers("/authenticate")
+                        .permitAll().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(new JWTAuthenticationFilter(useService, jwtTokenHelper),
+                        UsernamePasswordAuthenticationFilter.class);
+        http.cors();
+        http.csrf().disable().headers().frameOptions().disable();
         http.formLogin();
-        http.httpBasic();
+        // http.httpBasic();
     }
 }

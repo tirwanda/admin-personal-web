32bdb1028473a1a9b593a1606a10f0ed86e5fe00
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-09-14 14:38:20 +0700

    Add LoginResponse class

---
+++ be/src/main/java/com/admin/dashboard/be/response/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.admin.dashboard.be.response;
+
+public class LoginResponse {
+    private String token;
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}

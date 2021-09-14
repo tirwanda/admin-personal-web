55d2642fad14ec490ce7737cff0e637e73390d87
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-09-14 14:37:56 +0700

    Add AuthenticationRequest class

---
+++ be/src/main/java/com/admin/dashboard/be/request/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.admin.dashboard.be.request;
+
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}

732b9456cb516c07939c1139a631e47c37ff3dff
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-09-16 15:01:23 +0700

    Fixing conflict

--- be/src/main/java/com/admin/dashboard/be/BeApplication.java
+++ be/src/main/java/com/admin/dashboard/be/BeApplication.java
@@ -7,9 +7,6 @@ import com.admin.dashboard.be.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -20,7 +17,6 @@ public class BeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BeApplication.class, args);
 	}
-
 
 //	@Bean
 	CommandLineRunner run(UserService userService) {
--- be/src/main/java/com/admin/dashboard/be/entity/User.java
+++ be/src/main/java/com/admin/dashboard/be/entity/User.java
@@ -1,7 +1,6 @@
 package com.admin.dashboard.be.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -23,57 +22,6 @@ public class User {
     private String password;
     private String email;
 
-<<<<<<< HEAD
-    @ManyToMany(
-            cascade = CascadeType.ALL,
-            fetch = FetchType.EAGER
-    )
-    @JoinTable(
-            name = "AUTH_USER_AUTHORITY",
-            joinColumns = @JoinColumn(referencedColumnName = "userId"),
-            inverseJoinColumns = @JoinColumn(referencedColumnName = "id")
-    )
-    private List<Authority> authorities;
-
-    @Column(name = "enabled")
-    private boolean enabled=true;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    @Override
-    public String getPassword() {
-        return this.userPassword;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.userName;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return this.enabled;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return this.enabled;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return this.enabled;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return this.enabled;
-    }
-=======
     @ManyToMany(fetch = FetchType.EAGER)
     private Collection<Role> roles = new ArrayList<>();
->>>>>>> newLogin
 }

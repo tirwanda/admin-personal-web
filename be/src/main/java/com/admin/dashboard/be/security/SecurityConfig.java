63ab91409bd241a48e37a74953660d487e41fb95
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-09-18 21:26:44 +0700

    Finish Create Login sevice with JWT

--- be/src/main/java/com/admin/dashboard/be/security/SecurityConfig.java
+++ be/src/main/java/com/admin/dashboard/be/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/api/login").permitAll();
+        http.authorizeRequests().antMatchers("/api/login", "/api/token/refresh/**").permitAll();
         http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();

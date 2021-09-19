6973e54bec6eb0a9b76d48b7f072a01b01e7feda
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-09-19 23:02:12 +0700

    Create Context InitialState and Reducer for Authentication

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,14 +1,72 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect,
+} from 'react-router-dom';
+import { useReducer, createContext } from 'react';
 import './App.css';
 import LoginPage from './pages/LoginPage/LoginPage';
+import Dashboard from './pages/Dashboard/Dashboard';
+
+// Context
+export const AuthContext = createContext();
+
+// Initial State
+const initialState = {
+	isAuthenticated: false,
+	user: null,
+	token: null,
+};
+
+// Reducer
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'LOGIN':
+			localStorage.setItem('user', JSON.stringify(action.payload.user));
+			localStorage.setItem('token', JSON.stringify(action.payload.token));
+			return {
+				...state,
+				isAuthenticated: true,
+				user: action.payload.user,
+				token: action.payload.token,
+			};
+
+		case 'LOGOUT':
+			localStorage.clear();
+			return {
+				...state,
+				isAuthenticated: false,
+				user: action.payload.user,
+			};
+
+		default:
+			return state;
+	}
+};
 
 function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
 	return (
 		<div>
 			<Router>
 				<Switch>
-					<Route exact path="/" component={LoginPage} />
-					{/* <Route exact path="/" component={Resume} /> */}
+					<AuthContext.Provider
+						value={{
+							state,
+							dispatch,
+						}}
+					>
+						{!state.isAuthenticated ? (
+							<Redirect to={{ pathname: '/' }} />
+						) : (
+							<Redirect to={{ pathname: '/dashboard' }} />
+						)}
+
+						<Route exact path="/" component={LoginPage} />
+						<Route path="/dashboard" component={Dashboard} />
+					</AuthContext.Provider>
 				</Switch>
 			</Router>
 		</div>

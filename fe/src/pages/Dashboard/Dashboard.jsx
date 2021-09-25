d4e6672c9cbc66c061a6fcf6cc39037fcba54e67
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-09-25 23:20:45 +0700

    Create Dashboard only access when success login

--- fe/src/pages/Dashboard/Dashboard.jsx
+++ fe/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { fetchUserData } from '../../api/authenticationService';
+import { useHistory } from 'react-router';
 
-function Dashboard() {
+const MainWrapper = styled.div`
+	padding-top: 40px;
+`;
+
+function Dashboard(props) {
+	let history = useHistory();
+	const dispatch = useDispatch();
+	const [loading, setLoading] = useState(false);
+	const [data, setData] = useState({});
+
+	React.useEffect(() => {
+		fetchUserData()
+			.then((response) => {
+				setData(response.data);
+				console.log(response.data);
+			})
+			.catch((e) => {
+				localStorage.clear();
+				history.push('/');
+			});
+	}, []);
+
+	const logOut = () => {
+		localStorage.clear();
+		history.push('/');
+	};
+
 	return (
-		<div>
-			<h3>Wellcome to My dashboard</h3>
-		</div>
+		<Container>
+			<MainWrapper>
+				<h4>Hello {data[0] && `${data[0].name}`}</h4>
+				{data[0] &&
+					data[0].roles &&
+					data[0].roles.filter((value) => value.name === 'ROLE_USER')
+						.length > 0 && <Button type="variant">Add User</Button>}
+				<br></br>
+				<Button style={{ marginTop: '5px' }} onClick={() => logOut()}>
+					Logout
+				</Button>
+			</MainWrapper>
+		</Container>
 	);
 }
 

509b8f114257914f982e9d67c3f0cd86cf3ae902
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-10-09 19:36:50 +0700

    Finish Styling Navbar

--- fe/src/organism/Navbar/Navbar.jsx
+++ fe/src/organism/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './navbar.scss';
-import styled from 'styled-components';
 
 import { Search } from '@mui/icons-material';
 import { Container, Grid } from '@mui/material';
@@ -9,53 +8,23 @@ const Navbar = () => {
 	return (
 		<Container className="navbar">
 			<Grid container spacing={3} className="navbar-item">
-				<Grid item lg={8} md={8} className="navbar-text">
+				<Grid item lg={8} md={8} className="text">
 					<h2>
 						Good morning, <span>Edho Dwi Tirwanda</span>
 					</h2>
 				</Grid>
 
-				<Grid item lg={4} md={4}>
-					<InputContainer>
-						<Icon>
+				<Grid item lg={4} md={4} className="search">
+					<div className="input">
+						<div className="input-icon">
 							<Search />
-						</Icon>
-						<Input type="text" placeholder="Search for projects" />
-					</InputContainer>
+						</div>
+						<input type="text" placeholder="Search for projects" />
+					</div>
 				</Grid>
 			</Grid>
 		</Container>
 	);
 };
-
-const InputContainer = styled.div`
-	display: flex;
-`;
-
-const Icon = styled.div`
-	height: 3rem;
-	width: 3rem;
-	background-color: #dce4ff;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	border-top-left-radius: 0.5rem;
-	border-bottom-left-radius: 0.5rem;
-	svg {
-		color: #555555;
-	}
-`;
-const Input = styled.input`
-	border: none;
-	background-color: #dce4ff;
-	border-top-right-radius: 0.5rem;
-	border-bottom-right-radius: 0.5rem;
-	color: #464646;
-
-	&:focus {
-		border: none;
-		outline: none;
-	}
-`;
 
 export default Navbar;

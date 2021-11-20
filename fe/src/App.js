d3a0320499d087f62dc95f8a7652aa599125d27d
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-11-20 23:07:29 +0700

    Fixing Bugs in Routing

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,27 +2,28 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage/LoginPage';
 import Dashboard from './pages/Dashboard/Dashboard';
-import MainContent from './pages/MainContent/MainContent';
 import MainHome from './template/MainHome/MainHome';
 
 import GlobalStyle from './globalStyle';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme } from './atoms/Themes/Themes';
 import MainAbout from './template/MainAbout/MainAbout';
+import MainBlog from './template/MainBlog/MainBlog';
 
 function App() {
 	return (
 		<div>
 			<GlobalStyle />
 			<Router>
-				<Switch>
-					<ThemeProvider theme={lightTheme}>
+				<ThemeProvider theme={lightTheme}>
+					<Switch>
 						<Route exact path="/" component={MainHome} />
 						<Route exact path="/about" component={MainAbout} />
-					</ThemeProvider>
-					<Route exact path="/login" component={LoginPage} />
-					<Route path="/dashboard" component={Dashboard} />
-				</Switch>
+						<Route exact path="/blog" component={MainBlog} />
+						<Route exact path="/login" component={LoginPage} />
+						<Route path="/dashboard" component={Dashboard} />
+					</Switch>
+				</ThemeProvider>
 			</Router>
 		</div>
 	);

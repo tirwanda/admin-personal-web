3d0b0862787b6dbf833d3fcf5280bb9f0803df1f
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-11-20 22:24:44 +0700

    Modified and added Navlink to Logo

--- fe/src/atoms/Logo/LogoComponent.jsx
+++ fe/src/atoms/Logo/LogoComponent.jsx
@@ -1,19 +1,26 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { darkTheme } from '../Themes/Themes';
 
-const Logo = styled.h1`
+const Logo = styled(NavLink)`
 	display: inline-block;
+	font-size: 2rem;
 	color: ${(props) =>
 		props.color === 'dark' ? darkTheme.text : darkTheme.body};
 	font-family: 'Pacifico', cursive;
 	position: fixed;
 	left: 2rem;
 	top: 2rem;
+	text-decoration: none;
 	z-index: 3;
 `;
 
 const LogoComponent = (props) => {
-	return <Logo color={props.theme}>Tirwanda</Logo>;
+	return (
+		<Logo to="/" color={props.theme}>
+			Tirwanda
+		</Logo>
+	);
 };
 
 export default LogoComponent;

3fbb36ccf39dd8248807831bd3adaa9719c325ef
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-11-26 22:39:09 +0700

    Finis Create Testi for SaveTech Method

--- be/src/test/java/com/admin/dashboard/be/repository/TechRepositoryTest.java
+++ be/src/test/java/com/admin/dashboard/be/repository/TechRepositoryTest.java
@@ -1,17 +1,14 @@
 package com.admin.dashboard.be.repository;
 
 import com.admin.dashboard.be.entity.Tech;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
-@ExtendWith(SpringExtension.class)
 class TechRepositoryTest {
 
     @Autowired
@@ -23,5 +20,13 @@ class TechRepositoryTest {
     void injectedComponentsAreNotNull() {
         assertThat(techRepository).isNotNull();
         assertThat(projectRepository).isNotNull();
+    }
+
+    @Test
+    void testSaveTech() {
+        Tech tech = Tech.builder().name("Node").build();
+        techRepository.save(tech);
+
+        Assertions.assertThat(tech.getTechId()).isGreaterThan(0);
     }
 }

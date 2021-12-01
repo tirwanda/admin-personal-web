e2f5eebec8d2e6f8e5ab825cf859e50accf96ea4
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-12-01 13:40:31 +0700

    Finish Create unit test for userService

--- be/src/test/java/com/admin/dashboard/be/service/UserServiceImplTest.java
+++ be/src/test/java/com/admin/dashboard/be/service/UserServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.admin.dashboard.be.service;
 
-import com.admin.dashboard.be.entity.Role;
 import com.admin.dashboard.be.entity.User;
 import com.admin.dashboard.be.repository.ProfileImageRepository;
 import com.admin.dashboard.be.repository.ProjectRepository;
@@ -9,21 +8,19 @@ import com.admin.dashboard.be.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Collection;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 //@ExtendWith(MockitoExtension.class)
@@ -62,59 +59,46 @@ class UserServiceImplTest {
     }
 
     @Test
-    void loadUserByUsername() {
-        userService.getUser("tirwanda");
-        verify(userRepository).findByUsername("tirwanda");
-    }
-
-    @Test
-    void saveUser() {
-        User user = User.builder()
-                .name("Dwi T")
-                .username("dwi")
-                .password(passwordEncoder.encode("password"))
-                .email("dwi@mail.com")
-                .build();
-        userService.saveUser(user);
-        verify(userRepository, times(1)).save(user);
-    }
-
-    @Test
-    void updateUser() {
-        User user = User.builder()
-                .userId(10L)
-                .name("Dwi T")
-                .username("dwi")
-                .password(passwordEncoder.encode("password"))
-                .email("dwi@mail.com")
-                .build();
-        Mockito.when(userRepository.findByUsername("dwi")).thenReturn(user);
-
-        User actual = userService.updateUser(user);
-        assertThat(actual).isEqualTo(user);
-    }
-
-    @Test
-    void saveRole() {
-    }
-
-    @Test
-    void addRoleToUser() {
-    }
-
-    @Test
     void getUser() {
+        User expectUser = User.builder()
+                .username("tirwanda")
+                .name("Edho Dwi Tirwanda")
+                .email("edhodwtirwanda@gmail.com")
+                .about("My About")
+                .title("Backend Engineer")
+                .password(passwordEncoder.encode("password"))
+                .build();
+
+        when(userRepository.findByUsername("tirwanda")).thenReturn(expectUser);
+        User actualUser = userService.getUser("tirwanda");
+
+        assertThat(actualUser).isEqualTo(expectUser);
     }
 
     @Test
     void getUsers() {
+        User expectUser1 = User.builder()
+                .username("tirwanda")
+                .name("Edho Dwi Tirwanda")
+                .email("edhodwtirwanda@gmail.com")
+                .about("My About")
+                .title("Backend Engineer")
+                .password(passwordEncoder.encode("password"))
+                .build();
+
+        User expectUser2 = User.builder()
+                .username("dwi")
+                .name("Dwi Tirwanda")
+                .email("dwtirwanda@gmail.com")
+                .about("My About")
+                .title("Full Stack")
+                .password(passwordEncoder.encode("password"))
+                .build();
+
+        Mockito.when(userRepository.findAll()).thenReturn(Arrays.asList(expectUser1, expectUser2));
+
+        List<User> actualListOfUser = userService.getUsers();
+        assertThat(actualListOfUser).isEqualTo(Arrays.asList(expectUser1, expectUser2));
     }
 
-    @Test
-    void uploadUserProfileImage() {
-    }
-
-    @Test
-    void saveProjectToUser() {
-    }
 }

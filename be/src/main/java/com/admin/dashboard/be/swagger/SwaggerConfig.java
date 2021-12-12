2a950dd4b9a734cd46b6a7cd98bf43e3124583b8
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-12-12 21:59:06 +0700

    Modified API Documentation Info

--- be/src/main/java/com/admin/dashboard/be/swagger/SwaggerConfig.java
+++ be/src/main/java/com/admin/dashboard/be/swagger/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -16,8 +20,22 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.admin.dashboard.be.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Edho Dwi Tirwanda API",
+                "This is a API for Edho's Personal Website",
+                "v1.0",
+                "Terms of Service",
+                new Contact("Edho D. Tirwanda", "www.tirwanda.com", "edhodwitirwanda@gmail.com"),
+                "License of Tirwanda",
+                "www.tirwanda.com",
+                Collections.emptyList()
+        );
     }
 }
